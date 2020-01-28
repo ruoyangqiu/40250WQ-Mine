2c31a09940e54b71489002aee6a8d661cb74dfdf
Mike Koenig <koenigm@seattleu.edu>
2020-01-28 09:46:41 -0800

    Upated ItemModel

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -1,4 +1,9 @@
-﻿namespace Mine.Models
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using SQLite;
+
+namespace Mine.Models
 {
     /// <summary>
     /// Item for the Game
@@ -10,8 +15,50 @@
         // The Value of the item
         public int Value { get; set; } = 0;
 
+        [Ignore]
+        public List<History> AuditHistory { get; set; } = new List<History>();
+
+        // Holds the AuditHistory Json
+        public string AuditHistoryString { get; set; } = string.Empty;
+
+        public ItemModel() { }
+
+        public ItemModel (ItemModel data)
+        {
+            AuditHistoryString = data.AuditHistoryString;
+            AuditHistory = JsonConvert.DeserializeObject<List<History>>(AuditHistoryString);
+
+            // Update the Base
+            Id = data.Id;
+            Name = data.Name;
+            Description = data.Description;
+
+            // Update the extended
+            Value = data.Value;
+        }
+
         public bool Update(ItemModel data)
         {
+            var latest = JsonConvert.SerializeObject(data);
+            var previous = JsonConvert.SerializeObject(this);
+
+            AuditHistory = data.AuditHistory;
+
+            if (AuditHistory == null)
+            {
+                AuditHistory = new List<History>();
+            }
+
+            AuditHistory.Add(new History
+            {
+                Note = "Upated",
+                ChangedLatest = latest,
+                ChangedPrevious = previous,
+                ChangeSize = latest.Length - previous.Length
+            });
+
+            AuditHistoryString = JsonConvert.SerializeObject(AuditHistory);
+
             // Do not update the ID, if you do, the record will be orphaned
             // Id = data.Id;
 
@@ -24,5 +71,23 @@
 
             return true;
         }
+    }
+
+    public class History
+    {
+        // Change DateTime
+        public DateTime ChangeDateTime { get; set; } = DateTime.Now;
+        
+        // Comments about the Change
+        public string Note { get; set; } = "Note";
+       
+        // The Latest Record
+        public string ChangedLatest { get; set; } = string.Empty;
+        
+        // The Previous Record
+        public string ChangedPrevious { get; set; } = string.Empty;
+        
+        // The Size of the change between Latest - Prevous
+        public int ChangeSize { get; set; } = 0;
     }
 }
