23864f48a772f4140178e5fdedb801e8a45defbc
Mike Koenig <koenigm@seattleu.edu>
2020-02-06 09:56:13 -0800

    Added ItemIndexViewModelTests

---
+++ UnitTests/ViewModels/ItemIndexViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Mine.ViewModels;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+using Mine.Services;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace UnitTests.ViewModels
+{
+    public class ItemIndexViewModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            // Activate the Datastore
+            DependencyService.Register<MockDataStore>();
+        }
+
+        [Test]
+        public async Task ItemIndexViewModel_Read_Invalid_ID_Bogus_Should_Fail()
+        {
+            // Arrange
+
+            // Act
+            var result = await new ItemIndexViewModel().Read("bogus");
+
+            // Reset
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
