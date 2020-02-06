9f39d27a7a235680cb9f06ab2cc03eeb68b3c1fe
Mike Koenig <koenigm@seattleu.edu>
2020-02-06 09:56:33 -0800

    Added Read UT to ItemIndexViewModelTests

--- UnitTests/ViewModels/ItemIndexViewModelTests.cs
+++ UnitTests/ViewModels/ItemIndexViewModelTests.cs
@@ -33,5 +33,26 @@ namespace UnitTests.ViewModels
             // Assert
             Assert.IsNull(result);
         }
+
+        [Test]
+        public async Task ItemIndexViewModel_Read_Valid_ID_Bogus_Should_Pass()
+        {
+            // Arrange
+            var ViewModel = new ItemIndexViewModel();
+
+            // Load Data
+            ViewModel.LoadDatasetCommand.Execute(null);
+
+            // Find the First ID
+            var first = ViewModel.Dataset.FirstOrDefault();
+
+            // Act
+            var result = await new ItemIndexViewModel().Read(first.Id);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(result.Name, first.Name);
+        }
     }
 }
