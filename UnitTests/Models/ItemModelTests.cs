f14c581afbedb0e4bc701b677f70f225e97e0372
Mike Koenig <koenigm@seattleu.edu>
2020-02-05 22:46:18 -0800

    Added reference to Mine Project.

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+            // TODO: Add your test code here
+
+            // Act
+            var result = new ItemModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
+    }
+}
