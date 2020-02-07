677f55f76f89d99c3be68f8392489b98073a0870
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 15:40:58 -0800

    Added ItemUpdatePageTests

---
+++ UnitTests/Views/Items/ItemUpdatePageTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Views;
+using Mine.ViewModels;
+using Mine.Models;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class ItemUpdatePageTests
+    {
+        [Test]
+        public void ItemUpdatePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+            var ViewModel = new ItemViewModel();
+
+            // Act
+            var result = new ItemUpdatePage(ViewModel);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
