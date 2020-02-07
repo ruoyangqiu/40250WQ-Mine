672b4b2dc58ebd5f2040379ebe7804c794b4e35f
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 15:51:00 -0800

    Added MenuPageTests

---
+++ UnitTests/Views/MenuPageTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Views;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class MenuPageTests
+    {
+        [Test]
+        public void MenuPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            // Act
+            var result = new MenuPage();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
