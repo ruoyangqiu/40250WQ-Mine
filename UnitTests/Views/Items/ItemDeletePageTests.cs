6403a7f43fbefa1e7b374bbffcb12e562ccc8d4c
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 15:38:38 -0800

    Added ItemDeletePageTests

---
+++ UnitTests/Views/Items/ItemDeletePageTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Views;
+using Mine.ViewModels;
+using Mine.Models;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class ItemDeletePageTests
+    {
+        [Test]
+        public void ItemDeletePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+            var ViewModel = new ItemViewModel();
+
+            // Act
+            var result = new ItemDeletePage(ViewModel);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
