534a5c1c202b421b74cd7140cac4b5297ff83211
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 08:09:01 -0800

    Added BaseViewModelTests

---
+++ UnitTests/ViewModels/BaseViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.ViewModels;
+
+namespace UnitTests.ViewModels
+{
+    [TestFixture]
+    public class BaseViewModelTests
+    {
+        [Test]
+        public void BaseViewModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BaseViewModel();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
