eb7054e7fa4a04b1a282126bc1fbb970b9329d1e
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 15:44:53 -0800

    Added AboutPageTests

---
+++ UnitTests/Views/AboutPageTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Views;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class AboutPageTests
+    {
+        [Test]
+        public void AboutPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new AboutPage();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
