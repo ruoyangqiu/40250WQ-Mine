d19247d4ac763fb89fcff408b429aa86fc778f58
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 08:08:41 -0800

    Added BaseModelTests

---
+++ UnitTests/Models/BaseModelTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class BaseModelTests
+    {
+        [Test]
+        public void BaseModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BaseModel();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("This is an Item", result.Name);
+
+        }
+
+        [Test]
+        public void BaseModel_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var result = new BaseModel();
+
+            // Act
+            result.Id = "bogus";
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("bogus", result.Id);
+
+        }
+    }
+}
