d1e5b2bdbcd99747054ac459e2ff834487f0fade
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 15:36:50 -0800

    Added ItemCreatePageTests

---
+++ UnitTests/Views/Items/ItemCreatePageTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Views;
+using Mine.ViewModels;
+using Mine.Models;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class ItemCreatePageTests
+    {
+        [Test]
+        public void GamePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+            var ViewModel = new ItemViewModel();
+
+            // Act
+            var result = new ItemCreatePage(ViewModel);
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
