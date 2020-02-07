87ea9b701b150a65716c9c1dbebb2562877ce895
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 15:44:38 -0800

    Added ItemIndexPageTests

---
+++ UnitTests/Views/Items/ItemIndexPageTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Views;
+using Mine.ViewModels;
+using Mine.Models;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class ItemIndexPageTests
+    {
+        [Test]
+        public void ItemIndexPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemIndexPage();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
