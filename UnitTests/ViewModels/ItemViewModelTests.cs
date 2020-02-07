65554f28164e6b516d0ad213d359f7161c0e1987
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 15:11:05 -0800

    Added ItemViewModelTests

---
+++ UnitTests/ViewModels/ItemViewModelTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mine.Models;
+using Mine.ViewModels;
+
+namespace UnitTests.ViewModels
+{
+    [TestFixture]
+    public class ItemViewModelTests
+    {
+        [Test]
+        public void ItemViewModel_Constructor_Valid_Data_Should_Pass()
+        {
+            // Arrange
+            var data = new ItemModel();
+            data.Name = "Name";
+
+            // Act
+            var result = new ItemViewModel(data);
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Name", result.Data.Name);
+        }
+    }
+}
