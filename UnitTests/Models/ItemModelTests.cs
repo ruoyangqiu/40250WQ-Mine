e05ed03e144d6bb5c9c95dfac0451235f27d8644
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 08:09:22 -0800

    Updated ItemModelTests

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -16,8 +16,6 @@ namespace UnitTests.Models
         public void ItemModel_Constructor_Default_Should_Pass()
         {
             // Arrange
-            // TODO: Add your test code here
-
 
             // Act
             var result = new ItemModel();
@@ -26,6 +24,54 @@ namespace UnitTests.Models
 
             // Assert 
             Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void ItemModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result.Value);
+        }
+
+        [Test]
+        public void ItemModel_Set_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+            result.Value = 6;
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(6,result.Value);
+        }
+
+        [Test]
+        public void ItemModel_Update_Default_Should_Pass()
+        {
+            // Arrange
+            var dataOriginal = new ItemModel();
+            dataOriginal.Value = 1;
+            
+            var dataNew = new ItemModel();
+            dataNew.Value = 2;
+
+            // Act
+            var result = dataOriginal.Update(dataNew);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(2, dataOriginal.Value);
         }
     }
 }
