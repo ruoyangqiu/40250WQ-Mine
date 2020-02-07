c6b8784f94278cd1d918020dd1e46987ed7fae8e
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 08:28:03 -0800

    Added AboutViewModelTests

---
+++ UnitTests/ViewModels/AboutViewModelTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.ViewModels;
+
+namespace UnitTests.ViewModels
+{
+    [TestFixture]
+    public class AboutViewModelTests
+    {
+        [Test]
+        public void AboutViewModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new AboutViewModel();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void AboutViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new AboutViewModel();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("About", result.Title);
+        }
+    }
+}
