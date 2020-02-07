e243963f74c3235d50c601227bb1ea6f5893aab7
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 15:49:19 -0800

    Added MainPageTests (Not possible to cover the constructor)

---
+++ UnitTests/Views/MainPageTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Views;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class MainPageTests
+    {
+        // Not Possible to Cover the MainPage due to the call to MasterBehavior = MasterBehavior.Popover;
+
+        //[Test]
+        //public void MainPage_Constructor_Default_Should_Pass()
+        //{
+        //    // Arrange
+
+        //    // Initilize Xamarin Forms
+        //    MockForms.Init();
+
+        //    // Act
+        //    var result = new MainPage();
+
+        //    // Reset
+
+        //    // Assert
+        //    Assert.IsNotNull(result);
+        //}
+    }
+}
