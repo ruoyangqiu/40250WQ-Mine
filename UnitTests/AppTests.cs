37090df1ad1accd2639bf782c6b53e30d1e466fa
Mike Koenig <koenigm@seattleu.edu>
2020-02-07 16:04:21 -0800

    Added AppTests.cs

---
+++ UnitTests/AppTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mine.Views;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.Views.Game
+{
+    [TestFixture]
+    public class AppTests
+    {
+        // Not Possible to Cover the Page due to the call to MasterBehavior = MasterBehavior.Popover;
+
+        //[Test]
+        //public void App_Constructor_Default_Should_Pass()
+        //{
+        //    // Arrange
+
+        //    // Initilize Xamarin Forms
+        //    MockForms.Init();
+
+        //    // Act
+        //    var result = new Mine.App();
+
+        //    // Reset
+
+        //    // Assert
+        //    Assert.IsNotNull(result);
+        //}
+    }
+}
