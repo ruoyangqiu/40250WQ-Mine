6f5e38d7aa65d22e3830623d99689bb474b236f6
Mike Koenig <koenigm@seattleu.edu>
2020-02-09 22:33:12 -0800

    Added UT coverage to GamePage contenPage

--- UnitTests/Views/Game/GamePageTests.cs
+++ UnitTests/Views/Game/GamePageTests.cs
@@ -5,20 +5,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 using Mine.Views;
+using System.Reflection;
+using Xamarin.Forms.Mocks;
 
 namespace UnitTests.Views.Game
 {
     [TestFixture]
-    public class GamePageTests
+    public class GamePageTests : GamePage
     {
         [Test]
         public void GamePage_Constructor_Default_Should_Pass()
         {
             // Arrange
+            MockForms.Init();
+            var page = new GamePage();
 
             // Act
-            var result = new GamePage();
+            var result = page.Content;
 
             // Reset
 
@@ -40,6 +45,39 @@ namespace UnitTests.Views.Game
 
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void GamePage_Get_Default_Should_Pass()
+        {
+            // Arrange
+
+            var page = new GamePage();
+
+            // Act
+            var result = page.Content;
+
+            // Reset
+
+            // Assert
+            Assert.NotNull(result); 
+        }
+
+
+        [Test]
+        public void GamePage_OnBindingContextChanged_Default_Should_Pass()
+        {
+            // Arrange
+
+            var page = new GamePage();
+
+            // Act
+            OnBindingContextChanged();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true);
         }
     }
 }
