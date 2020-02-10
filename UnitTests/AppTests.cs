ffc0cee41779a3fd26a28646bc96659a2b871fe8
Mike Koenig <koenigm@seattleu.edu>
2020-02-09 21:07:51 -0800

    Added UT Coverage for App

--- UnitTests/AppTests.cs
+++ UnitTests/AppTests.cs
@@ -7,29 +7,100 @@ using System.Text;
 using System.Threading.Tasks;
 using Mine.Views;
 using Xamarin.Forms.Mocks;
+using Mine;
 
 namespace UnitTests.Views.Game
 {
     [TestFixture]
-    public class AppTests
+    public class AppTests : App
     {
-        // Not Possible to Cover the Page due to the call to MasterBehavior = MasterBehavior.Popover;
+        [Test]
+        public void App_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            // Act
+            var result = new Mine.App();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void App_OnResume_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Initilize Xamarin Forms
+            MockForms.Init();
+            var page = new Mine.App();
+
+            // Act
+            OnResume();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void App_OnSleep_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Initilize Xamarin Forms
+            MockForms.Init();
+            var page = new Mine.App();
+
+            // Act
+            OnSleep();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void App_OnStart_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Initilize Xamarin Forms
+            MockForms.Init();
+            var page = new Mine.App();
+
+            // Act
+            OnStart();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
 
         //[Test]
-        //public void App_Constructor_Default_Should_Pass()
+        //public void App_InitializeComponent_Default_Should_Pass()
         //{
-        //    // Arrange
+        //    Arrange
 
-        //    // Initilize Xamarin Forms
+        //    Initilize Xamarin Forms
         //    MockForms.Init();
+        //    var page = new Mine.App();
 
-        //    // Act
-        //    var result = new Mine.App();
+        //    Act
+        //    InitializeComponent();
 
-        //    // Reset
+        //    Reset
 
-        //    // Assert
-        //    Assert.IsNotNull(result);
+        //    Assert
+        //    Assert.IsTrue(true); // Got to here, so it happened...
         //}
     }
 }
