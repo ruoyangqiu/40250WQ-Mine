96bf7f12790d62508877368afbb4a5bbe068f6af
Mike Koenig <koenigm@seattleu.edu>
2020-02-09 21:25:33 -0800

    Catch Null condition in ItemIndex Update method

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -123,6 +123,11 @@ namespace Mine.ViewModels
         /// <returns></returns>
         public async Task<bool> Update(ItemModel data)
         {
+            if (data == null)
+            {
+                return false;
+            }
+
             // Check that the record exists, if it does not, then exit with false
             var record = await Read(data.Id);
             if (record == null)
